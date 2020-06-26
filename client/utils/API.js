3484f462854b22ed4bb95d36d75812310345b4f1
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-26 16:44:35 -0700

    Add a method to get data from google book api on API

--- client/utils/API.js
+++ client/utils/API.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 
+// Google Book API REF)
+// https://developers.google.com/books/docs/v1/using
+// https://developers.google.com/books/docs/v1/reference
+
+const API_KEY = process.env.GOOGLE_API_KEY;
+const googleBookURI = `https://www.googleapis.com/books/v1/`
+
 export default {
+  // Gets all books from google book api by a keyword
+  getBooks: function(keyword) {
+    return axios.get(`${googleBookURI}/volumes?q=${keyword}?key=${API_KEY}`);
+  },
   // Gets all saved books
   getSavedBooks: function() {
     return axios.get("/api/books");
