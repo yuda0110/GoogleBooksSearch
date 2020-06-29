48eca0e6f8e4ed3bedaeef3cacc8be1983938b25
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-29 16:54:37 -0700

    Render saved books on the Saved page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import DeleteBtn from "../components/Btn";
+import DeleteBtn, {SaveBtn, ViewBtn} from "../components/Btn";
 import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
 import Header from "../components/Header";
 import { Input, FormBtn } from "../components/Form";
+import {List, ListItem} from "../components/List";
 
 class Saved extends Component {
   state = {
@@ -32,6 +33,26 @@ class Saved extends Component {
         <Row>
           <Col size="md-12">
             <h3>Saved Books</h3>
+            {this.state.savedBooks.length ? (
+              <List>
+                {this.state.savedBooks.map(book => (
+                  <ListItem key={book._id}>
+                    <p>Title: {book.title}</p>
+                    <p>Authors: {book.authors.join(', ')}</p>
+                    <img src={book.image} alt={book.title}/>
+                    <p>Description: {book.description}</p>
+                    <ViewBtn link={book.link} />
+                    {/*<SaveBtn*/}
+                    {/*  savedBookIds={this.state.savedBookIds}*/}
+                    {/*  bookId={book.id}*/}
+                    {/*  saveHandler={this.handleSave(book)}*/}
+                    {/*/>*/}
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <p>No Results to Display</p>
+            )}
           </Col>
         </Row>
       </Container>
