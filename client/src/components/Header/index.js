a42e4012a4aa3a828602f0f8b75abedec20c675b
Yuko Uda <yuko.uda0110@gmail.com>
2020-06-30 17:05:17 -0700

    Add React Logo to the header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.css";
+import Icon from "../../assets/images/react-logo.svg"
 
 const Header = () => {
   return(
     <header>
-      <h1>(React) Google Books Search</h1>
+      <h1>
+        <img src={Icon} alt="React" className="react-icon"/>
+        Google Books Search
+      </h1>
       <h2>Search for and Save Books of Internet</h2>
     </header>
   );
